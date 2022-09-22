34f209ae7de2ca85dbe3880545753108c401cd1e
Moshood Gbadamosi <gbadamosimoshood@ymail.com>
2022-09-22 19:08:26 +0100

    New Changes on Nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -741,6 +741,10 @@ export default {
   mounted() {
     window.scrollTo(0, 0);
     this.getAllproducts();
+    
+        let externalScriptCustom = document.createElement('script')
+        externalScriptCustom.setAttribute('src', 'https://cdn.statically.io/gh/NathTimi/Mart-script/main/custom.js')
+        document.head.appendChild(externalScriptCustom)
   },
   methods: {
     async getAllproducts() {
