5278dcd0fef018c8400826d2725af6a3850abd90
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2022-09-23 13:41:22 +0100

    Update mainFooter.vue

--- src/views/mainFooter.vue
+++ src/views/mainFooter.vue
@@ -2,13 +2,26 @@
   <footer class="mainFooter">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-md-3 Hide">
+          <div class="footerCol">
+            <div class="pmg">
+              <img src="@/assets/img/pmlg.png" alt="">
+            </div>
+            <div class="aligm-icon">
+              <div class="media-icons">
+                <a href="https://www.facebook.com/producemartcom"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/producemartdotcom/"><i class="fab fa-twitter"></i></a>
+                <a href="https://twitter.com/producemartcom"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/producemart-com/about/?viewAsMember=true"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
           <div class="footerCol">
             <h4>Company</h4>
             <div class="divText">
-              <p>
-                Producemart.com is a brand new online B2B marketplace for wholesale agricultural produce and ingredients. It aims to make it simpler than ever to list and source produce at a local, regional or international level.
-              </p>
+              <p><router-link to="/about"><a>About Us</a></router-link></p>
             </div>
             <div class="divText">
               <i class="bi bi-geo-alt"></i> 128 City Road, London, EC1V 2NX
@@ -25,12 +38,17 @@
         <div class="col-md-2">
           <div class="footerCol">
             <h4>Services</h4>
-            <p><router-link to="/about"><a>About Us</a></router-link></p>
             <!-- <p><router-link to="/contact"><a>Contact Us</a></router-link></p> -->
+            <p><router-link to="/products"><a>View Product</a></router-link></p>
             <p><router-link to="/request-a-product"><a>Request a Product</a></router-link></p>
+            <h4 class="mt-4">Resources</h4>
+            <!-- <p><router-link to="#"><a>Ebook Download</a></router-link></p>
+            <p><router-link to="#"><a>Import Export Guide</a></router-link></p> -->
+            <p><router-link to="/blog"><a>Blog</a></router-link></p>
+            <p><router-link to="/faq"><a>FAQs</a></router-link></p>
           </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-2">
           <div class="footerCol">
             <h4>Legal</h4>
             <p><router-link to="/terms"><a>Terms & Conditions</a></router-link></p>
@@ -38,15 +56,20 @@
             <p><router-link to="/cookies"><a>Cookies Policy</a></router-link></p>
           </div>
         </div>
-        <div class="col-md-2">
-          <div class="footerCol">
-            <h4>Resources</h4>
-            <!-- <p><router-link to="#"><a>Ebook Download</a></router-link></p>
-            <p><router-link to="#"><a>Import Export Guide</a></router-link></p> -->
-            <p><router-link to="/blog"><a>Blog</a></router-link></p>
-            <p><router-link to="/faq"><a>FAQs</a></router-link></p>
+        <div class="col-md-3">
+          <div class="footerCol mobile">
+            <div class="pmg">
+              <img src="@/assets/img/pmlg.png" alt="">
+            </div>
+            <div class="media-icons">
+              <a href="https://www.facebook.com/producemartcom"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://twitter.com/producemartcom"><i class="fab fa-twitter"></i></a>
+              <a href="https://www.instagram.com/producemartdotcom/"><i class="fab fa-instagram"></i></a>
+              <a href="https://www.linkedin.com/company/producemart-com/about/?viewAsMember=true"><i class="fab fa-linkedin-in"></i></a>
+            </div>
           </div>
         </div>
+        <div class="col-md-1"></div>
       </div>
     </div>
     <div class="container copyrightFooter">
@@ -64,7 +87,88 @@
 </template>
 <style scoped src="@/assets/css/styleFrontend.css"></style>
 <style scoped src="@/assets/css/bootstrap.css"></style>
-
+<style>
+  .footerCol .pmg img{
+    width: 80%;
+  }
+.media-icons{
+    display: flex;
+}
+.Hide{
+  display: none;
+}
+@media(max-width: 1299px){
+  .mobile{
+    display: none;
+  }
+  .Hide{
+  display: block;
+}
+}
+@media(max-width: 767px){
+  .footerCol .pmg img{
+    width: 50%;
+    display: block;
+    margin: auto;
+  }
+  .aligm-icon{
+    display: block !important;
+    margin: auto !important;
+    width: 40% ;
+  }
+}
+@media(max-width: 520px){
+  .aligm-icon{
+    display: block !important;
+    margin: auto !important;
+    width: 50%;
+  }
+}
+ .media-icons a{
+    height: 40px;
+    width: 40px;
+    margin: 0 8px;
+    border-radius: 10%;
+    text-align: center;
+    line-height: 40px;
+    color: #fff;
+    font-size: 17px;
+    text-decoration: none;
+    transition: all 0.4s ease;
+}
+.media-icons a:nth-child(1){
+  background: #fff;
+  color: #4267B2 !important;
+}
+.media-icons a:nth-child(1):hover{
+  color: #fff !important;
+  background: #4267B2;
+}
+.media-icons a:nth-child(2){
+  background: #fff;
+    color: #1DA1F2 !important;
+}
+.media-icons a:nth-child(2):hover{
+    color: #fff !important;
+    background: #1DA1F2;
+}
+.media-icons a:nth-child(3){
+  color:#E1306C !important;
+  background: #fff;
+}
+.media-icons a:nth-child(3):hover{
+    color: #fff !important;
+    background: #E1306C;
+}
+.media-icons a:nth-child(4){
+  color: #0077B5 !important;
+    background: #fff;
+}
+.media-icons a:nth-child(4):hover{
+    color: #fff !important;
+    background: #0077B5;
+}
+</style>
 <script>
   import ScrollTopArrow from '@/components/scrollArrow'
   export default {
